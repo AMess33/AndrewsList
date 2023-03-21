e7839f765495b38048a4a3dedf3e4855f4853bb5
Alan Hornbaker <alanhornbaker@gmail.com>
2023-03-20 21:27:06 -0500

    Created schema.sql.  Started models.  Copied over index.js, need to lay out associations between models.  Need to create seed files for database tables.

---
+++ models/client.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Client extends Model {}
+
+Client.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      //   type: DataTypes.DATE,
+      //   allowNull: false,
+      //   defaultValue: DataTypes.NOW,
+    },
+    email: {
+      type: DataTypes.string,
+      allowNull: false,
+    },
+    phonenumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.string,
+      allowNull: false,
+    },
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "client",
+  }
+);
+
+module.exports = Client;
---
+++ models/contractor.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Contractor extends Model {}
+
+Contractor.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      //   type: DataTypes.DATE,
+      //   allowNull: false,
+      //   defaultValue: DataTypes.NOW,
+    },
+    email: {
+      type: DataTypes.string,
+      allowNull: false,
+    },
+    phonenumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "contractor",
+  }
+);
+
+module.exports = Contractor;
