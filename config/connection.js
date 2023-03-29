56408869844ae2018058b9670a39330e06582b7d
AMess33 <amess3309@gmail.com>
2023-03-28 20:23:50 -0500

    update connections

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,21 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "127.0.0.1",
+      dialect: "mysql",
+      port: 3301,
+    }
+  );
+}
 
 module.exports = sequelize;
