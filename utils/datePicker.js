140aa0ac7978ecebcac50ec07ca5209dfa19e1f8
AMess33 <amess3309@gmail.com>
2023-03-29 15:10:49 -0500

    fnish newProject create project handlebars add date-picker npm

---
+++ utils/datePicker.js
@@ -0,0 +1,7 @@
+const datepicker = require("js-datepicker");
+
+const start = document.querySelector("#startDate");
+const startDate = datepicker("#startDate");
+
+const end = document.querySelector("#endDate");
+const endDate = datepicker("#endDate");
