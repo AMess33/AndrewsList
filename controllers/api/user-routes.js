83e6ac0916f4b1fb66b83681e3d9d607b54213dc
AMess33 <amess3309@gmail.com>
2023-03-30 16:54:58 -0500

    fix pathing in signin

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 // user post route
-router.post("/user", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
