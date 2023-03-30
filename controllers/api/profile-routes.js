630e9b1da1102391eef946c3a7e0d45de5ec0f4c
AMess33 <amess3309@gmail.com>
2023-03-30 18:09:03 -0500

    get routes to work and applicatino running as intended

--- controllers/api/profile-routes.js
+++ controllers/api/profile-routes.js
@@ -15,26 +15,23 @@ router.get("/project", (req, res) => {
 });
 
 // // get route for projects...findall and join with user
-router.get("/profile", withAuth, async (req, res) => {
-  try {
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
-    });
-    const projects = projectData.map((project) => project.get({ plain: true }));
-    res.render("profile"),
-      {
-        projects,
-        logged_in: req.session.logged_in,
-      };
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get("/", withAuth, async (req, res) => {
+// try {
+//   const projectData = await Project.findAll({
+//     // where: {
+//     //   user_id: req.session.user_id,
+//     // },
+//   });
+//   const projects = projectData.map((project) => project.get({ plain: true }));
+//   console.log(projectData);
+//   res.render("profile", {
+//     projects,
+//     logged_in: req.session.logged_in,
+//   });
+// } catch (err) {
+//   res.status(500).json(err);
+// }
+// });
 
 // currently not needed.
 // router.delete('/:id', withAuth, async (req, res) => {
