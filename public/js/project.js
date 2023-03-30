b4cf018c1f83318729da03387c9759b5df6e05df
AMess33 <amess3309@gmail.com>
2023-03-30 16:44:41 -0500

    fix typos and routing

--- public/js/project.js
+++ public/js/project.js
@@ -46,6 +46,6 @@ document
   .querySelector(".new-project-form")
   .addEventListener("submit", newFormHandler);
 
-document
-  .querySelector(".project-list")
-  .addEventListener("click", delButtonHandler);
+// document
+//   .querySelector(".project-list")
+//   .addEventListener("click", delButtonHandler);
