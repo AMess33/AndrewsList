10453e2c0072875c7bfb9e703c1fb6892538081f
Alan Hornbaker <alanhornbaker@gmail.com>
2023-03-30 22:13:25 -0500

    Cleaned out unneeded comments, deleted unused productTag.js file

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,4 @@
 const withAuth = (req, res, next) => {
-  // TODO: Add a comment describing the functionality of this if statement
-  // REdirect user to login page if the logged_in status is false.
   if (!req.session.logged_in) {
     res.redirect("/login");
   } else {
